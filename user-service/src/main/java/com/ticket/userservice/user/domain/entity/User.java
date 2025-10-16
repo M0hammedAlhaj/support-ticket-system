d503c268ed3c6231be9cff42b956849efe1619b4
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-10-16 16:23:28 +0300

    feat(user-service):add id and uuid

--- user-service/src/main/java/com/ticket/userservice/user/domain/entity/User.java
+++ user-service/src/main/java/com/ticket/userservice/user/domain/entity/User.java
@@ -1,13 +1,26 @@
 package com.ticket.userservice.user.domain.entity;
 
 import com.ticket.userservice.user.domain.model.UserType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
+@Document(collation = "users")
+@Data
 public class User {
 
-    private UUID id;
+    @Id
+    private String id;
+
+    private UUID uuid;
+
+    private String firstName;
+
+    private String lastName;
 
     private String email;
 
@@ -18,4 +31,15 @@ public class User {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- user-service/src/main/java/com/ticket/userservice/user/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/ticket/userservice/user/domain/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.ticket.userservice.user.domain.repository;
 
+import com.ticket.userservice.user.domain.entity.User;
+
 public interface UserRepository {
+
+    void save(User user);
 }
