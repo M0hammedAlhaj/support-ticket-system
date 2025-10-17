ec051ca361ea896f3cfe7e797d8deaf5297d4362
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-10-17 14:16:01 +0300

    fix(user-service):injection

--- user-service/src/main/java/com/ticket/userservice/user/infrastructre/repository/MongoUserRepositoryAdapter.java
+++ user-service/src/main/java/com/ticket/userservice/user/infrastructre/repository/MongoUserRepositoryAdapter.java
@@ -2,13 +2,15 @@ package com.ticket.userservice.user.infrastructre.repository;
 
 import com.ticket.userservice.user.domain.entity.User;
 import com.ticket.userservice.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class MongoUserRepositoryAdapter implements UserRepository {
-    private SpringMongoUserRepository springMongoUserRepository;
+    private final SpringMongoUserRepository springMongoUserRepository;
 
     @Override
     public void save(User user) {
