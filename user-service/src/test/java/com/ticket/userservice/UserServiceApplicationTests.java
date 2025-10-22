e3cd6b588b64a73f81f414e041258c2f7969ed17
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-10-22 17:48:23 +0300

    feat(ci/cd):add container test

--- user-service/src/test/java/com/ticket/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/ticket/userservice/UserServiceApplicationTests.java
@@ -2,9 +2,33 @@ package com.ticket.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0")
+            .withExposedPorts(27017);
+
+    @Container
+    static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management-alpine")
+            .withExposedPorts(5672);
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
+        registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
+    }
 
     @Test
     void contextLoads() {
